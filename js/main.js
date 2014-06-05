490b610308d99d0bf746b15689034357e9b7dde1
Charlie Chen <conechan@gmail.com>
2014-06-05 11:42:15 +0800

    Adjusted archive.html, added jQuery animation.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
+'use strict';
+jQuery(function($){
+  $('.archive ul').addClass('hidden');
 
+  var currentTime = new Date($.now());
+  var currentYear = currentTime.getFullYear();
+
+  $('.archive ul').each(function(i, elem) {
+    if ($(this).attr('id').replace('year', '') == currentYear) {
+      $(this).removeClass('hidden');
+    }
+  });
+
+  $('.archive-year').on('click', function(){
+    var ulYearID = '#year' + $(this).text();
+    var archiveYearID = '#y' + $(this).text();
+    $('.archive ul').addClass('hidden');
+    $(ulYearID).removeClass('hidden');
+    $('html, body').animate({
+      scrollTop: $(archiveYearID).offset().top - 20
+    }, 100);
+  });
+});
